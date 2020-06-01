d1e48b72f81bf599979e5dc10648191bc6253953
HolyHades <33263529+HolyHades@users.noreply.github.com>
2020-06-01 12:20:06 +0800

    please kill me now

--- de/gallery/scripts.js
+++ de/gallery/scripts.js
@@ -8,26 +8,132 @@ function closeNav() {
 
 function openModal1(){
     var modal = document.getElementById("modal1");
-    var img = document.getElementById("img01");
     var modalImg = document.getElementById("image01");
     modal.style.display = "block";
-    modalImg.src = "/Logo-Groß-Grau.png";
+    modalImg.src = "/photos/pic1.jpg";
 }
 
 function openModal2(){
     var modal = document.getElementById("modal2");
-    var img = document.getElementById("img02");
     var modalImg = document.getElementById("image02");
     modal.style.display = "block";
-    modalImg.src = "/Logo-Groß-Weiß.png";
+    modalImg.src = "/photos/pic2.jpg";
+}
+
+function openModal3(){
+    var modal = document.getElementById("modal3");
+    var modalImg = document.getElementById("image03");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic3.jpg";
+}
+
+function openModal4(){
+    var modal = document.getElementById("modal4");
+    var modalImg = document.getElementById("image04");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic4.jpg";
+}
+
+function openModal5(){
+    var modal = document.getElementById("modal5");
+    var modalImg = document.getElementById("image05");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic5.jpg";
+}
+
+function openModal6(){
+    var modal = document.getElementById("modal6");
+    var modalImg = document.getElementById("image06");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic6.jpg";
+}
+
+function openModal7(){
+    var modal = document.getElementById("modal7");
+    var modalImg = document.getElementById("image07");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic7.jpg";
+}
+
+function openModal8(){
+    var modal = document.getElementById("modal8");
+    var modalImg = document.getElementById("image08");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic8.jpg";
+}
+
+function openModal9(){
+    var modal = document.getElementById("modal9");
+    var modalImg = document.getElementById("image09");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic9.jpg";
+}
+
+function openModal10(){
+    var modal = document.getElementById("modal10");
+    var modalImg = document.getElementById("image10");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic10.jpg";
+}
+
+function openModal11(){
+    var modal = document.getElementById("modal11");
+    var modalImg = document.getElementById("image11");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic11.jpg";
+}
+
+function openModal12(){
+    var modal = document.getElementById("modal12");
+    var modalImg = document.getElementById("image12");
+    modal.style.display = "block";
+    modalImg.src = "/photos/pic12.jpg";
 }
 
 function close1() {
-    var span = document.getElementById("close1")[0];
     modal1.style.display = "none";
 }
 
 function close2() {
-    var span = document.getElementById("close2")[0];
     modal2.style.display = "none";
+}
+
+function close3() {
+    modal3.style.display = "none";
+}
+
+function close4() {
+    modal4.style.display = "none";
+}
+
+function close5() {
+    modal5.style.display = "none";
+}
+
+function close6() {
+    modal6.style.display = "none";
+}
+
+function close7() {
+    modal7.style.display = "none";
+}
+
+function close8() {
+    modal8.style.display = "none";
+}
+
+function close9() {
+    modal9.style.display = "none";
+}
+
+function close10() {
+    modal10.style.display = "none";
+}
+
+function close11() {
+    modal11.style.display = "none";
+}
+
+function close12() {
+    modal12.style.display = "none";
 }
